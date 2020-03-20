2a6c9501788ff9ad29c155829cfefe6494bcd233
pannaAwesome <pannaogpiko@gmail.com>
2020-03-20 12:04:03 +0100

    Added poker game play rounds

--- app.js
+++ app.js
@@ -1,4 +1,28 @@
-var http = require('http');
+let http = require('http');
+const fs = require ('fs')
+const port = 3000 
+//ip adress 46.30.215.207
+// domain peter3010.dk
+// port 200
+ 
+const server = http.createServer((req, res) => {
+    res.writeHead(200,{ 'content-type': 'text/html' });
+    fs.readFile('index.html', (error, data) => {
+        if(error === null) {
+            res.write(data);
+            res.end();
+        } else {
+            res.writeHead(404);
+            res.write('error: fil ikke fundet');
+            res.end();
+        }
+    });
+});
 
-http.createServer(function (req, res) {
-}).listen(8080);
+server.listen(port, function(error){
+    if(error){
+        console.log('Noget gik galt',error);
+    } else {
+      console.log('serveren svarer' + port);
+    }
+});
--- js/game.js
+++ js/game.js
@@ -8,12 +8,18 @@ class Card {
 let screenWidth = document.screenWidth;
 let screenHeight = document.screenHeight;
 
+var blind = 10;
+var player_balance = 100;
+
 var testCard = new Card(10, 3);
 var player_hand = [testCard, testCard];
-var open_hand = [testCard, testCard, testCard];
+var flop = [testCard, testCard, testCard];
+var turn = [testCard, testCard, testCard, testCard];
+var river = [testCard, testCard, testCard, testCard, testCard];
 
 let color = ["heart.png","clover.png","spade.png","diamond.png"];
 
+//####################################################################################################
 function createCard(card_to_create){
     let cardColor = document.createElement("img");
     cardColor.src = "./Images/" + color[card_to_create.suit];
@@ -33,19 +39,100 @@ function createBotCard(){
     return cardHolder;
 }
 
-function givePlayerCards(){
-    for (let index = 0; index < player_hand.length; index++) {
-        let card = createCard(player_hand[index]);
-        document.getElementById("player-cards").appendChild(card);
-    }
+function deleteCards(parent_id) {
+    let parent = document.getElementById(parent_id);
     
-    document.getElementById("bot-cards").appendChild(createBotCard());
-    document.getElementById("bot-cards").appendChild(createBotCard());
+    parent.querySelectorAll("*").forEach(child => child.remove());
 }
 
-function giveTableCards(){
-    for (let index = 0; index < open_hand.length; index++) {
-        let card = createCard(open_hand[index]);
+function showJumbotron(text) {
+    document.getElementById("jumbo-text").innerHTML = text;
+    document.getElementById("jumbotron").style.visibility = "visible";
+}
+
+function hideJumbotron() {
+    document.getElementById("jumbotron").style.visibility = "hidden";
+}
+
+function showButtons(robot_move) {
+    document.getElementById("player-options").style.visibility = "visible";
+    switch (robot_move.move) {
+        case "check":
+            
+            break;
+    
+        default:
+            break;
+    }
+}
+
+function hideButtons() {
+    document.getElementById("player-options").style.visibility = "hidden";
+}
+//####################################################################################################
+function setStartup(balance, bb) {
+    let balance_div = document.getElementById("balance-field");
+    balance_div.innerHTML = (balance-bb) + "$";
+
+    let pot_div = document.getElementById("pot");
+    pot_div.innerHTML = (bb+bb/2) + "$";
+}
+
+function givePlayerCards(player_cards){
+    deleteCards("player-cards");
+    deleteCards("bot-cards");
+
+    for (let index = 0; index < player_cards.length; index++) {
+        let card = createCard(player_cards[index]);
+        document.getElementById("player-cards").appendChild(card);
+        document.getElementById("bot-cards").appendChild(createBotCard());
+    }
+}
+
+function giveTableCards(table_cards){
+    deleteCards("open-cards");
+
+    for (let index = 0; index < table_cards.length; index++) {
+        let card = createCard(table_cards[index]);
         document.getElementById("open-cards").appendChild(card);
     }
+}
+
+function getPokerbotPlay() {
+    let bot_move = {move: "raise", amount: 30};
+    return bot_move;
+}
+
+function showMove(current_move) {
+    let text = "Pokerbot has ";
+    switch (current_move.move) {
+        case "check":
+            showJumbotron(text + "checked");
+            break;
+        case "call":
+            showJumbotron(text + "called");
+            break;
+        case "fold":
+            showJumbotron(text + "folded");
+            break;
+        case "raise":
+            showJumbotron(text + "raised with " + current_move.amount);
+            break;
+        default:
+            break;
+    }
+}
+
+//####################################################################################################
+window.onload = function() {
+    this.setStartup(this.player_balance, this.blind);
+    this.givePlayerCards(player_hand);
+
+    this.showJumbotron("Pokerbots turn");
+    this.setTimeout(this.hideJumbotron, 2000);
+    let pokerBot_move = this.getPokerbotPlay();
+    this.setTimeout(this.showMove, 4000, pokerBot_move);
+    this.setTimeout(this.hideJumbotron, 6000);
+
+
 }
