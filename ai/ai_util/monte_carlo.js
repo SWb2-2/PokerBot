94e70d8652f492d54f32483482d1ec234387ae04
fhyldg18 <57632309+fhyldg18@users.noreply.github.com>
2020-04-28 21:35:59 +0200

    updateret tabel

--- ai/ai_util/monte_carlo.js
+++ ai/ai_util/monte_carlo.js
@@ -2,8 +2,10 @@ const Dealer = require("../../website/js/classes/dealer.js");
 const Player = require("../../website/js/classes/player.js");
 const Card = require("../../website/js/classes/card.js")
 
+
+
 //Gives the calculated equity, of a given suited hand. 
-const arr_suit =   [ undefined, 
+/*const arr_suit =   [ undefined, 
                      undefined, 
                     [undefined, undefined, undefined, 38.28, 39.06, 40.00, 39.72, 40.07, 42.07, 44.48, 47.11, 49.77, 52.74, 55.76, 59.84],
                     [undefined, undefined, 38.28, undefined, 40.75, 41.79, 41.47, 41.94, 42.68, 45.29, 47.91, 50.58, 53.46, 56.55, 60.58],
@@ -37,9 +39,43 @@ const arr_o_suit = [ undefined,
                     [undefined, undefined, 53.31, 54.07, 54.97, 55.88, 56.58, 57.35, 57.91, 59.74, 61.87, 62.67, 63.64, 82.82, 67.55],
                     [undefined, undefined, 57.55, 58.41, 59.16, 60.13, 59.80, 60.92, 61.71, 62.60, 64.72, 65.55, 66.54, 67.55, 85.84]
                     ]
+*/
+const arr_suit = /*00*/  [ undefined, 
+                 /*01*/    undefined, 
+                 /*02*/   [undefined, undefined, undefined, 35.23, 35.98, 36.97, 36.74, 37.33, 39.45, 41.98, 44.70, 47.55, 50.61, 53.79, 57.85],
+                 /*03*/   [undefined, undefined, 35.23, undefined, 37.75, 38.73, 38.50, 39.08, 40.06, 42.76, 45.52, 48.31, 51.39, 54.61, 58.55],
+                 /*04*/   [undefined, undefined, 35.98, 37.75, undefined, 40.37, 40.18, 40.87, 41.76, 43.28, 46.28, 49.17, 52.15, 55.33, 59.30],
+                 /*05*/   [undefined, undefined, 36.97, 38.73, 40.37, undefined, 41.94, 42.55, 43.53, 45.05, 46.93, 50.01, 52.99, 56.21, 60.26],
+                 /*06*/   [undefined, undefined, 36.74, 38.50, 40.18, 41.94, undefined, 44.18, 45.18, 46.58, 48.62, 50.58, 53.73, 56.91, 60.20],
+                 /*07*/   [undefined, undefined, 37.33, 39.08, 40.87, 42.55, 44.18, undefined, 46.89, 48.35, 50.26, 52.22, 54.43, 57.85, 61.23],
+                 /*08*/   [undefined, undefined, 39.45, 40.06, 41.76, 43.53, 45.18, 46.89, undefined, 50.06, 51.85, 53.90, 56.20, 58.59, 62.18],
+                 /*09*/   [undefined, undefined, 41.98, 42.76, 43.28, 45.05, 46.58, 48.35, 50.06, undefined, 53.87, 55.78, 58.04, 60.47, 63.29],
+                 /*10*/   [undefined, undefined, 44.70, 45.52, 46.28, 46.93, 48.62, 50.26, 51.95, 53.87, undefined, 57.94, 60.10, 62.63, 65.34],
+                 /*11*/   [undefined, undefined, 47.55, 48.31, 49.17, 50.01, 50.58, 52.22, 53.90, 55.78, 57.94, undefined, 61.06, 63.47, 66.32],
+                 /*12*/   [undefined, undefined, 50.61, 51.39, 52.15, 52.99, 53.73, 54.43, 56.20, 58.04, 60.10, 61.06, undefined, 64.48, 67.30],
+                 /*13*/   [undefined, undefined, 53.79, 54.61, 55.33, 56.21, 56.91, 57.85, 58.59, 60.47, 62.63, 63.47, 64.48, undefined, 68.40],
+                 /*14*/   [undefined, undefined, 57.85, 58.55, 59.30, 60.26, 60.20, 61.23, 62.18, 63.29, 65.34, 66.32, 67.30, 68.40, undefined]
+                    ]
 
+//Gives the calculated equity, of a given off suit hand. 
+const arr_o_suit = /*00*/[ undefined, 
+                   /*01*/  undefined,			 
+                   /*02*/ [undefined, undefined, 50.00, 31.61, 32.41, 33.50, 33.17, 33.79, 36.11, 38.55, 41.69, 44.55, 47.80, 51.18, 55.41],
+                   /*03*/ [undefined, undefined, 31.61, 32.41, 34.27, 35.36, 35.15, 35.73, 36.71, 39.56, 42.48, 45.42, 48.57, 52.03, 56.31],
+                   /*04*/ [undefined, undefined, 32.41, 34.27, 56.50, 37.16, 36.94, 37.58, 38.54, 40.14, 43.38, 46.30, 49.44, 52.91, 57.11],
+                   /*05*/ [undefined, undefined, 33.50, 35.46, 37.16, 59.69, 38.81, 39.40, 40.40, 42.03, 44.00, 47.22, 50.52, 53.72, 58.01],
+                   /*06*/ [undefined, undefined, 33.17, 35.15, 36.94, 38.81, 62.57, 41.17, 42.15, 43.74, 45.73, 47.82, 51.25, 54.62, 58.01],
+                   /*07*/ [undefined, undefined, 33.79, 35.73, 37.58, 39.40, 41.17, 65.50, 44.00, 45.54, 47.56, 49.57, 52.00, 55.54, 59.08],
+                   /*08*/ [undefined, undefined, 36.11, 36.71, 38.54, 40.40, 42.15, 44.00, 68.43, 47.42, 49.38, 51.38, 53.67, 56.35, 60.12],
+                   /*09*/ [undefined, undefined, 38.55, 39.56, 40.14, 42.03, 43.74, 45.54, 47.42, 71.55, 51.48, 53.41, 55.77, 58.39, 61.23],
+                   /*10*/ [undefined, undefined, 41.69, 42.48, 43.38, 44.00, 45.73, 47.56, 49.38, 51.48, 74.79, 55.78, 58.00, 60.54, 63.53],
+                   /*11*/ [undefined, undefined, 44.55, 45.42, 46.30, 47.22, 47.82, 49.57, 51.38, 53.41, 55.78, 77.42, 58.94, 61.51, 64.53],
+                   /*12*/ [undefined, undefined, 47.80, 48.57, 49.44, 50.52, 51.25, 52.00, 53.67, 55.77, 58.00, 58.94, 80.01, 62.62, 65.54],
+                   /*13*/ [undefined, undefined, 51.18, 52.03, 52.91, 53.72, 54.62, 55.54, 56.35, 58.39, 60.54, 61.51, 62.62, 82.63, 66.67],
+                   /*14*/ [undefined, undefined, 55.41, 56.31, 57.11, 58.01, 58.01, 59.08, 60.12, 61.23, 63.53, 64.53, 65.54, 66.67, 85.54]
+                    ]
 
-// calculate_preflop();
+calculate_preflop();
 
 // console.log(equity_range([new Card(4,2), new Card(2,2)], 100000,[], 30, 100));
 
@@ -231,20 +267,24 @@ function equity_range(hand, numb_of_sim, table, range_low, range_high) {
 
 //Finds the winrate for all the possible hands in preflop, 
 function calculate_preflop() {
-        
+	print_suit = [];
+	print_pair = [];
+	print_offsuit = [];
     let result_suit = [];
     let result_pair = [];
     let result_off_suit = [];
+
 
     //Find suit winrate 
     for(let i = 2; i <= 14; i++) {
         for(let k = i; k <= 14; k++) {
             if(k != i) {
                 let current_winner = equity_range([new Card(i, 0), new Card(k, 0)], 2000000);
-                result_suit.push({card_1: i, card_2: k, w_r: current_winner.draw_and_winrate});
+				result_suit.push({card_1: i, card_2: k, w_r: current_winner.draw_and_winrate});
             }
         }
-    }
+	}
+	
     console.log("\n\n SUIT WINRATE", result_suit, "\n\n");
     
     //Find pair and off suit winrate 
@@ -258,7 +298,8 @@ function calculate_preflop() {
             }
         }
     }
-    console.log("\n\nOFF SUIT WINRATE", result_off_suit, "\n\nPAIRS", result_pair);
+	console.log("\n\nOFF SUIT WINRATE", result_off_suit, "\n\nPAIRS", result_pair);
+	
 }
 
 module.exports.calculate_preflop = calculate_preflop;
